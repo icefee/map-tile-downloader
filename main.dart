23ae5ed131c702f53bfac489234e1357df51cb1d
ying_123 <zhangying@aten-optics.com>
2023-01-09 15:26:51 +0800

    add download progress.

--- main.dart
+++ main.dart
@@ -1,24 +1,13 @@
 import 'dart:math';
 import 'dart:io';
+import 'package:console_bars/console_bars.dart';
 import './tool/map.dart';
 import './tool/tile.dart';
 import './tool/downloader.dart';
 
-int thread = 20;
-
 void main(List<String> args) {
-  // var point = MapTool.lngLatToTile(113.238831, 23.281719, 14);
-  // print('${point.x}, ${point.y}');
-
-  // String url = 'http://wallpaperswide.com/download/cute_anime_girl_2-wallpaper-3554x1999.jpg';
-  // downloadFile(url);
-  // Future.wait([randomInt(2), randomInt(5)]).then((value) => print(value));
-  downloadTiles(
-      parseLocationArg(args[0]),
-      parseLocationArg(args[1]),
-      parseRangeArg(args[2]),
-      types: args.length > 3 ? parseMapType(args[3]) : null
-  );
+  downloadTiles(parseLocationArg(args[0]), parseLocationArg(args[1]),
+      parseRangeArg(args[2]), parseMapType(args[3]), parseThread(args[4]));
 }
 
 Location parseLocationArg(String arg) {
@@ -45,10 +34,9 @@ List<MapTileType> parseMapType(String arg) {
   }).toList();
 }
 
-Future<int> randomInt(int seconds) async {
-  var random = Random();
-  await Future.delayed(Duration(seconds: seconds));
-  return random.nextInt(1200);
+int parseThread(String arg) {
+  List<String> params = arg.split('=');
+  return int.parse(params.last);
 }
 
 Future<void> downloadFile(String url) async {
@@ -73,7 +61,7 @@ String loopCreateDir(String path, {String basePath = '.'}) {
     if (dir.existsSync()) {
       continue;
     }
-    print('create $currentPath');
+    // print('create $currentPath');
     dir.create();
   }
   return dir.path;
@@ -83,17 +71,14 @@ Future<void> loopDownloadTile(String url, String fileName) async {
   try {
     await Downloader.downloadFile(url, fileName: fileName);
   } catch (err) {
-    print('$url 下载失败, 重试中...');
+    // print('$url 下载失败, 重试中...');
     return loopDownloadTile(url, fileName);
   }
 }
 
-Future<void> downloadTile(int x, int y, int z,
-    {List<MapTileType>? types}) async {
-  print('pending { x: $x, y: $y, z: $z }');
-  await Future.wait<void>(
-      (types ?? [MapTileType.Normal, MapTileType.Sate, MapTileType.Mix])
-          .map((MapTileType mapType) {
+Future<void> downloadTile(int x, int y, int z, List<MapTileType> types) async {
+  // print('pending { x: $x, y: $y, z: $z }');
+  await Future.wait<void>(types.map((MapTileType mapType) {
     String fileName = loopCreateDir('tiles/${mapType.value}/$z/$x');
     return loopDownloadTile(
         MapTile.getTileUrl(mapType, x: x, y: y, z: z), fileName + '/$y.png');
@@ -101,18 +86,28 @@ Future<void> downloadTile(int x, int y, int z,
 }
 
 Future<void> downloadTiles(Location loc1, Location loc2, List<int> range,
-    {List<MapTileType>? types}) async {
-  for (int z = range[0]; z <= range[1]; z++) {
+    List<MapTileType> types, int thread) async {
+  FillingBar fillingBar = FillingBar(
+      desc: '地图下载中',
+      total: range.last - range.first + 1,
+      time: true,
+      percentage: true,
+      space: '-',
+      fill: '=',
+      width: 20
+  );
+  for (int z = range.first; z <= range.last; z ++) {
     List<Tile> tiles = getTiles(loc1, loc2, z);
-    for (int x = tiles[0].x; x <= tiles[1].x; x++) {
-      int start = tiles[0].y, end = tiles[1].y;
-      for (int i = 0; i <= ((end - start) / thread).floor(); i++) {
+    for (int x = tiles.first.x; x <= tiles.last.x; x ++) {
+      int start = tiles.first.y, end = tiles.last.y;
+      for (int i = 0; i <= ((end - start) / thread).floor(); i ++) {
         int from = start + i * thread,
             to = min(end, start + (i + 1) * thread - 1);
-        await Future.wait<void>(List.generate(to - from + 1,
-            (int s) => downloadTile(x, from + s, z, types: types)));
-        // await threadFeature(start + i * thread, min(end, start + (i + 1) * thread - 1));
+        await Future.wait<void>(List.generate(
+            to - from + 1, (int s) => downloadTile(x, from + s, z, types)));
       }
     }
+    fillingBar.increment();
   }
+  print('地图下载完成.');
 }
