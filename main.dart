c371f46e844d04fd7a4382ddaebece8c9be35e35
ying_123 <zhangying@aten-optics.com>
2023-03-13 13:27:22 +0800

    更新

--- main.dart
+++ main.dart
@@ -6,17 +6,19 @@ void main(List<String> args) {
     
       ┏┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┓
       │                                           │
-      │         map tile downloader v1.3          │
-      │             build 2023.02.16              │
+      │         map tile downloader v1.31         │
+      │             build 2023.03.13              │
       │         https://map-tile.surge.sh         │
       │                                           │
       ┗┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┛
       
       ┏┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┓
+      
         如果你对参数不熟悉, 请尽量通过生成的批处理文件(.bat)下载.
         访问: https://map-tile.surge.sh 生成
-        备用地址: https://cif.stormkit.dev/maptile
+        备用地址: https://c.stormkit.dev/maptile
         确保生成的批处理文件跟本程序在同一目录, 且本程序的名称为 tile.exe
+        
       ┗┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┛
       
     ''');
@@ -36,42 +38,32 @@ void main(List<String> args) {
         print('发现配置文件map.xml, 读取参数中..');
         Location? location = readConfigFromXml(file);
         if (location != null) {
-          int radius = 20;
+          int radius = 10;
           print('参数读取成功, 矩形选定范围 $radius km, 开始下载.');
-          Location leftTop = Location(location.lng - lngInKm * radius, location.lat - latInKm * radius);
-          Location rightBottom = Location(location.lng + lngInKm * radius, location.lat + latInKm * radius);
-          downloadTiles(
-            DownloadConfig(
-              leftTop,
-              rightBottom,
+          Location leftTop = Location(
+              location.lng - lngInKm * radius, location.lat - latInKm * radius);
+          Location rightBottom = Location(
+              location.lng + lngInKm * radius, location.lat + latInKm * radius);
+          downloadTiles(DownloadConfig(leftTop, rightBottom,
               range: [3, 19],
               types: parseMapType('normal,sate,mix'),
-              thread: 40
-            )
-          );
+              thread: 40));
+        } else {
+          throw Exception('经纬度读取失败');
         }
-        else {
-          throw Exception('read lng lat fail');
-        }
+      } else {
+        throw Exception('配置文件读取失败');
       }
-      else {
-        throw Exception('read xml error');
-      }
-    }
-    catch (err) {
+    } catch (err) {
       print(err);
       print('读取参数失败, 请手动指定参数');
       getInputArgs();
     }
   } else {
-    downloadTiles(
-      DownloadConfig(
-        parseLocationArg(args[0]),
-        parseLocationArg(args[1]),
+    downloadTiles(DownloadConfig(
+        parseLocationArg(args[0]), parseLocationArg(args[1]),
         range: parseRangeArg(args[2]),
         types: parseMapType(args[3]),
-        thread: parseThread(args[4])
-      )
-    );
+        thread: parseThread(args[4])));
   }
 }
